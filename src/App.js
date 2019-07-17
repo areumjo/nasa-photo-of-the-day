7f7f97f9dec3542e4fa71e6c3263ded0956d8c03
Areum Jo <areumjo1@gmail.com>
2019-07-17 15:05:00 -0400

    Initial commit: project set up and import React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 
+import ImgContainer from "./Component/ImgContainer.js"
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +10,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <ImgContainer />
     </div>
   );
 }
---
+++ src/Component/ImgContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function IamgeContainer () {
+    console.log('check working?');
+    return (
+        <div>
+            <p>testing</p>
+        </div>
+    )
+}
+
+export default IamgeContainer;
