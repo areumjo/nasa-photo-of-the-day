49c4b88e6927767d740c9b47688c3bd5f5525fa6
Areum Jo <areumjo1@gmail.com>
2019-07-17 15:39:13 -0400

    Fetched NASA APOD DATA and displyed it

--- src/Component/ImgContainer.js
+++ src/Component/ImgContainer.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios"; 
 
 function IamgeContainer () {
-    console.log('check working?');
+    const [images, setImages] = useState('');
+    const [dateInfo, setDateInfo] = useState('2012-03-14');
+    useEffect( () => {
+        axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-12`)
+            .then(response => {
+                console.log(response.data)
+                const imgUrl = response.data.url;
+                setImages(imgUrl);
+                const imgDate = response.data.date;
+            })
+            .catch(err => console.log("NASA API is down"))
+    }, [])
+
     return (
         <div>
-            <p>testing</p>
+            <img src={images} alt="NASA APOD"/>
+            
         </div>
     )
 }
